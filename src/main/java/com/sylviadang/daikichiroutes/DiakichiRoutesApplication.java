5d4b05bded6328aae70b36fce9e0972ec86d8a15
Your Name <email@example.com>
2023-08-04 14:54:33 -0700

    finished

---
+++ src/main/java/com/sylviadang/daikichiroutes/DiakichiRoutesApplication.java
@@ -0,0 +1,15 @@
+package com.sylviadang.daikichiroutes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class DiakichiRoutesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiakichiRoutesApplication.class, args);
+	}
+	
+	
+}
---
+++ src/main/java/com/sylviadang/daikichiroutes/HomeController.java
@@ -0,0 +1,23 @@
+package com.sylviadang.daikichiroutes;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping("/daikichi")
+public class HomeController {
+	@RequestMapping("")
+	public String welome() {
+		return "welcome";
+	}
+	
+	@RequestMapping("/today")
+	public String today() {
+		return "Today you will find luck in all your endeavors!";
+	}
+	
+	@RequestMapping("/tomorrow")
+	public String tomorrow() {
+		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+	}
+	
+}
